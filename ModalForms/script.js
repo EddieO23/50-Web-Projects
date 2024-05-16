b46b05af6739b4f88dad57af714ea43827150c24
eddie <eocampo52@gmail.com>
2024-05-16 08:29:27 -0500

    finished landing page commit

--- ModalForms/script.js
+++ ModalForms/script.js
@@ -10,11 +10,30 @@ const loginX = document.querySelector(".login-x")
 const signupBtn = document.querySelector(".signup-btn")
 const loginBtn = document.querySelector(".login-btn")
 
+const formContainter = document.querySelector(".form-container")
+
+
+
 signupBtn.addEventListener("click", () => {
     signupModal.classList.add('display')
+    formContainter.classList.add('disable')
 })
 
 loginBtn.addEventListener("click", () => {
     loginModal.classList.add('display')
+    formContainter.classList.add('disable')
+
+})
+
+signupX.addEventListener("click", () => {
+    signupModal.classList.remove('display')
+    formContainter.classList.remove('disable')
+
+})
+
+loginX.addEventListener("click", () => {
+    loginModal.classList.remove('display')
+    formContainter.classList.remove('disable')
+
 })
 
